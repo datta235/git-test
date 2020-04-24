cbe4d93e1ce82031781b6833bc61fbb05c6d4ece
datta235 <saidatta1432015@gmail.com>
2020-04-24 10:51:11 +0530

    extra feature in substract function

--- substract.js
+++ substract.js
@@ -1,3 +1,9 @@
-function subtract(a, b) {
-   return a-b;
+// return subtraction of two numbers
+function subtract(a, b, bMINUSa) {
+    if (bMINUSa === true) {
+        return b - a;
+    }
+    else {
+        return a - b;
+    }
 };
